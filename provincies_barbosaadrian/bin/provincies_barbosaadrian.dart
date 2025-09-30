ef9294fa8e32b373e0c2cb4a967b1dce00ef9264
adriianbarbosa <adrian.bou04@gmail.com>
2025-09-30 15:02:23 +0200

    Proyecte creat

---
+++ provincies_barbosaadrian/bin/provincies_barbosaadrian.dart
@@ -0,0 +1,5 @@
+import 'package:provincies_barbosaadrian/provincies_barbosaadrian.dart' as provincies_barbosaadrian;
+
+void main(List<String> arguments) {
+  print('Hello world: ${provincies_barbosaadrian.calculate()}!');
+}
